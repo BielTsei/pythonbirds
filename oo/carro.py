5007307d4cc39f3a1058c1d6b909a0ea0e3c88f1
BielTsei <tseigabriel@gmail.com>
2022-06-22 14:13:52 -0300

    Implementado carro

--- oo/carro.py
+++ oo/carro.py
@@ -99,6 +99,30 @@ OESTE = 'Oeste'
 SUL = 'Sul'
 
 
+class Carro:
+    def __init__(self, direcao, motor):
+        self.direcao = direcao
+        self.motor = motor
+
+    def calcular_velocidade(self):
+        return self.motor.velocidade
+
+    def acelerar(self):
+        self.motor.acelerar()
+
+    def frear(self):
+        self.motor.frear()
+
+    def calcular_direcao(self):
+        return self.direcao.valor
+
+    def girar_a_direita(self):
+        self.direcao.girar_a_direita()
+
+    def girar_a_esquerda(self):
+        self.direcao.girar_a_esquerda()
+
+
 class Motor:
     def __init__(self):
         self.velocidade = 0
@@ -115,15 +139,11 @@ class Direcao:
     rotacao_a_direita_dct = {NORTE: LESTE, LESTE: SUL, SUL: OESTE, OESTE: NORTE}
     rotacao_a_esquerda_dct = {NORTE: OESTE, OESTE: SUL, SUL: LESTE, LESTE: NORTE}
 
-
     def __init__(self):
         self.valor = NORTE
-
 
     def girar_a_direita(self):
         self.valor = self.rotacao_a_direita_dct[self.valor]
 
-
     def girar_a_esquerda(self):
         self.valor = self.rotacao_a_esquerda_dct[self.valor]
-
